ed324dda8ae09a3a79a345b755d55678fcfd34e7
PaoGon <pickelse25@gmail.com>
2022-10-09 21:06:00 +0800

    initialize git repo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import './App.css';
+
+const App:FC = () =>{
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
