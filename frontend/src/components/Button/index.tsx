916f2f8386b040f8a85b8e52b9fa5e61f8c62841
gitmccoyabrasaldo <mccoyabrasaldo@gmail.com>
2022-12-09 22:28:02 +0800

    Update Button component

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,36 +1,48 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import "./style.css";
 import { ButtonProps } from "./models";
 
-const STYLES = ["btn--primary", "buton--secondary"]
+const STYLES = [
+  "btn--primary",
+  "btn--secondary",
+  "btn--categories",
+  "btn--selector",
+];
 
-
-const SIZES = ["btn--large", "btn--medium", "btn--small"]
-
-
+const SIZES = [
+  "btn--large",
+  "btn--medium",
+  "btn--small",
+  "for-icons",
+  "btn--next",
+];
 
 const Button: FC<ButtonProps> = ({
-    children,
-    onClick,
-    type,
-    buttonStyle,
-    buttonSize
-
+  icon,
+  svg,
+  children,
+  onClick,
+  type,
+  buttonStyle,
+  buttonSize,
 }) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-
-    return (
-        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-         onClick={onClick}
-         type={"button"}>
-        
-         {children}
-         </button>
-    );
-} 
-
+  return (
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onClick}
+      type={"button"}
+    >
+      {svg ? <img src={svg} alt="icon" className="btn-img" /> : ""}
+      {icon}
+      {children}
+    </button>
+  );
+};
 
 export default Button;
